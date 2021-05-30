747f7b92d596410fd7c1b49c53e7fade124c8152
Josh Brown <brownjoshalan@gmail.com>
2021-05-30 10:59:42 -0600

    Added dependencies that I was missing

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-//const routes = require("./controllers");
-const helpers = require("./utils/helpers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create the Handlebars.js engine object with custom helper functions
+// Set up sessions with cookies
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({ helpers });
 
-// Inform Express.js which template engine we're using
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
