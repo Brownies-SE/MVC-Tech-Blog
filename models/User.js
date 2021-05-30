83cc24b21fb1901b96560317b51e4373524832d0
Josh Brown <brownjoshalan@gmail.com>
2021-05-30 10:48:39 -0600

    created the user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true, //Email has to be a unique email. Cannot use the same email more than once.
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6], //6 charcaters long minimum
+    },
+  },
+  sequelize,
+  timestamps: true,
+  freezeTableName: false,
+  underscored: true,
+  modelName: "user",
+});
