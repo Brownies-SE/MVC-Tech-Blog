8273932b6376d928c4910e5581f929400a4df902
Josh Brown <brownjoshalan@gmail.com>
2021-06-02 17:57:30 -0600

    Changed the layout

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,18 +2,47 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
-router.post("/:post_id", async (req, res) => {
-  console.log(req.session);
-  if (req.session.loggedIn) {
-    req.body.user_id = req.session.user_id;
-    const pData = await Post.findByPk(req.params.post_id);
-    req.body.post_id = pData.id;
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
-    const cData = await Comment.create(req.body);
-    res.json(pData);
-  } else {
-    res.redirect("/login");
-  }
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    post_id: req.body.post_id,
+    // use the id from the session
+    user_id: req.session.user_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+//for deleting comments
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No comment found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
